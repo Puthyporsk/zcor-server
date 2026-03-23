6ebdd48fe95a101b6cc6c90cc8cf9708a1279395
Puthypor Sengkeo <sengkeoputhypor@gmail.com>
2026-03-23 00:24:35 -0700

    pfp upload file size limit handler

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ dotenv.config({ path: join(__dirname, ".env") });
 
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
@@ -34,8 +33,7 @@ app.set("view engine", "ejs");
 
 app.use(helmet());
 app.use(express.json({ limit: "10mb" }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://zcor.org", "https://www.zcor.org"],
@@ -43,7 +41,6 @@ app.use(
   }),
 );
 app.options("*", cors());
-app.use(express.json());
 app.use(cookieParser());
 
 // Rate limiting on auth endpoints
